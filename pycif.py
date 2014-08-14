c2ed471eced2a8eef69a3cdd47703bf4a0e1fc8f
Nicola DiPalma <nickyd242@gmail.com>
2014-08-14 00:50:53 -0700

    Initial setup.

---
+++ pycif.py
@@ -0,0 +1,16 @@
+# Description: Allows user to convert International Union of Crystallography (IUCr) Crystallographic Information Format (CIF) files to JSON for use with external modules.
+# Developer: Nicola B. DiPalma
+# Scripted using Python v3.4.1
+# For use with Python v3.x
+
+# Dependencies
+
+
+# script body for file processing
+def main():
+
+
+
+# Call main function
+if __name__ == "__main__":
+	main()
---
+++ pycif_spec.py
@@ -0,0 +1,21 @@
+# Description: Allows user to convert International Union of Crystallography (IUCr) Crystallographic Information Format (CIF) files to JSON for use with external modules.
+# Developer: Nicola B. DiPalma
+# Scripted using Python v3.4.1
+# For use with Python v3.x
+
+# Dependencies
+import pycif
+import unittest
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+	def setUp(self):
+		# test initializers
+
+	def test_shuffle(self):
+		# first test
+
+# Run unit tests
+if __name__ == '__main__':
+	unittest.main()

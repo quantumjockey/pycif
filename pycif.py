8e7a72065fd66354275ac47deb8716c7ea79bf8a
Nicola DiPalma <nickyd242@gmail.com>
2014-08-28 16:41:10 -0700

    Implemented extraction for basic cell properties.
    (To be converted from if/elif to dictionaries.)

--- pycif.py
+++ pycif.py
@@ -7,6 +7,8 @@
 import argparse
 import sys
 
+from mineral import Mineral
+
 
 # script body for file processing
 def main():
@@ -28,10 +30,65 @@ def main():
 	if args.rawFormatted:
 		PrintRawData(rawFileContent, True)
 
+	ProcessFile(rawFileContent)
+
 
 # Parse the text file data
-def ProcessTextFile(sourceFile):
-	fileContent = []
+def ProcessFile(rawContent):
+	filteredContent = []
+	for line in rawContent:
+		if " " in line:
+			filteredContent.append(line.split())
+		else:
+			filteredContent.append(line)
+	mineralData = Mineral()
+
+	# Get chemical properties
+	for data in filteredContent:
+		if (data[0] == "_chemical_name_mineral"):
+			mineralData.chemical_name = data[1]
+		elif (data[0] == "_chemical_formula_sum"):
+			mineralData.chemical_formula_sum = data[1]
+
+	# Get journal params
+	for data in filteredContent:
+		if (data[0] == "_journal_name_full"):
+			mineralData.journal_name = data[1]
+		elif (data[0] == "_journal_volume"):
+			mineralData.journal_volume = data[1]
+		elif (data[0] == "_journal_year"):
+			mineralData.journal_year = data[1]
+		elif (data[0] == "_journal_page_first"):
+			mineralData.journal_page_first = data[1]
+		elif (data[0] == "_journal_page_last"):
+			mineralData.journal_page_last = data[1]
+		elif (data[0] == "_publ_section_title"):
+			mineralData.journal_section_title = data[1]
+
+	# Get database code
+	for data in filteredContent:
+		if (data[0] == "_database_code_amcsd"):
+			mineralData.amcsd_database_code = data[1]
+
+	# Get unit cell params
+	for data in filteredContent:
+		if (data[0] == "_cell_length_a"):
+			mineralData.cell_length_a = data[1]
+		elif (data[0] == "_cell_length_b"):
+			mineralData.cell_length_b = data[1]
+		elif (data[0] == "_cell_length_c"):
+			mineralData.cell_length_c = data[1]
+		elif (data[0] == "_cell_angle_alpha"):
+			mineralData.cell_angle_alpha = data[1]
+		elif (data[0] == "_cell_angle_beta"):
+			mineralData.cell_angle_beta = data[1]
+		elif (data[0] == "_cell_angle_gamma"):
+			mineralData.cell_angle_gamma = data[1]
+
+	# Get crystal density diffraction
+	for data in filteredContent:
+		if (data[0] == "_exptl_crystal_density_diffrn"):
+			mineralData.exptl_crystal_density_diffrn = data[1]
 
 
 # Reads raw file data and retrieves each line in string format

2db6501f8ec14d3ed9d488d4bac771f9dc9f8198
Nicola DiPalma <nickyd242@gmail.com>
2014-08-28 14:06:40 -0700

    Added options to print file data before processing.

--- pycif.py
+++ pycif.py
@@ -5,25 +5,55 @@
 
 # Dependencies
 import argparse
+import sys
 
 
 # script body for file processing
 def main():
 
 	parser = argparse.ArgumentParser()
+
 	parser.add_argument('infile', type=argparse.FileType('r'), default=sys.stdin, help='File to be parsed.')
+	parser.add_argument("-r", "--raw", help="Prints raw file data (unformatted) before processing.", action="store_true")
+	parser.add_argument("-rf", "--rawFormatted", help="Prints raw file data (formatted) before processing.", action="store_true")
+
 	args = parser.parse_args()
 
 	fileData = args.infile
 	sourceFile = fileData.name
 
+	rawFileContent = GetRawData(fileData)
+	if args.raw:
+		PrintRawData(rawFileContent, False)
+	if args.rawFormatted:
+		PrintRawData(rawFileContent, True)
+
 
 # Parse the text file data
-def ProcessTextFile(sourceFile, targetFileDirectory, extension):
-	lineOfData = []
+def ProcessTextFile(sourceFile):
+	fileContent = []
 
-	for line in sourceFile:
-		lineOfData = line.split()
+
+# Reads raw file data and retrieves each line in string format
+def GetRawData(fileStream):
+	fileContent = []
+	for line in fileStream:
+		filteredLine = line.strip()
+		if filteredLine != "":
+			fileContent.append(filteredLine)
+	return fileContent
+
+
+# Prints raw file data obtained from a string array
+def PrintRawData(rawDataArray, shouldFormat):
+	print("Raw file data:")
+	if shouldFormat:
+		print("------------------------------------------")
+		for data in rawDataArray:
+			print(data)
+		print("------------------------------------------")
+	else:
+		print(rawDataArray)
 
 
 # Call main function
